e7db5c1da7c9ec6808b3b36622555d3a9e3b01ad
Raghavendra Dubey <36820299+raghav-dubey-avid@users.noreply.github.com>
2019-06-08 00:45:47 +0530

    Add files via upload

---
+++ Project/ZigZagGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+	public bool gameOver;
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame(){
+		UIManager.instance.GameStart ();
+		ScoreManager.instance.StartScore ();
+	}
+
+	public void GameOver(){
+		UIManager.instance.GameOver ();
+		ScoreManager.instance.StopScore ();
+		gameOver = true;
+	}
+}
---
+++ Project/ZigZagGame/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour {
+
+	public GameObject platform;
+	public GameObject diamond;
+	Vector3 lastPos;
+	float size;
+	public bool gameOver;
+
+
+	// Use this for initialization
+	void Start () {
+		lastPos = platform.transform.position;
+		size = platform.transform.localScale.x;
+
+		for (int i = 0; i < 20; i++) {
+			SpawnPlatforms ();
+		}
+
+		InvokeRepeating ("SpawnPlatforms", 0.1f, 0.2f);
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+		if (GameManager.instance.gameOver == true) {
+			CancelInvoke ("SpawnPlatforms");
+		}
+	}
+
+	void SpawnPlatforms(){
+		
+		int rand = Random.Range (0, 6);
+		if (rand < 3) {
+			SpawnX ();
+		} else if (rand >= 3) {
+			SpawnZ ();
+		}
+	
+	}
+
+	void SpawnX(){
+
+		Vector3 pos = lastPos;
+		pos.x += size;			//adding size to x position
+		lastPos = pos;
+		Instantiate (platform, pos, Quaternion.identity);
+
+		int rand = Random.Range (0, 4);
+		if (rand < 1) {
+			Instantiate (diamond, new Vector3(pos.x,pos.y + 1,pos.z), diamond.transform.rotation);
+		}
+			
+	}
+
+	void SpawnZ(){
+		Vector3 pos = lastPos;
+		pos.z += size;			//adding size to z position
+		lastPos = pos;
+		Instantiate (platform, pos, Quaternion.identity);
+
+		int rand = Random.Range (0, 4);
+		if (rand < 1) {
+			Instantiate (diamond, new Vector3(pos.x,pos.y + 1,pos.z), diamond.transform.rotation);
+		}
+	
+	}
+
+}
